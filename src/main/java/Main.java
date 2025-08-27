8cfd3a20fdfdad58d67e9013927c42709da51c4f
Alexdev-sudo <ezequielcastm123@gmail.com>
2025-08-27 11:12:29 -0600

    Commit: modificacion de main, seteo de menu de ipciones

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,99 @@
 import Entidades.*;
 import Gestiones.*;
 import Persistencia.GestorJSON;
+import java.time.LocalDate;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-    
+    Gestor_Atletas gestor = new Gestor_Atletas();
+    GestorEstadistica estadisticas = new GestorEstadistica();
+    GestorJSON json = new GestorJSON();
+
+    // cargar datos previos
+        var atletascargados = json.cargar();
+        if (atletascargados != null) {
+            atletascargados.forEach(gestor :: agregar);
+        }
+        Scanner leer = new Scanner(System.in);
+        int opcion;
+        do{
+            System.out.println("\n---sistema de control de rendimiento de atletas---");
+            System.out.println("1. registrar nuevo atleta");
+            System.out.println("2. registrar entrenamiento");
+            System.out.println("3. Mostrar historial de un atleta");
+            System.out.println("4. Mostrar estadisticas de un atleta");
+            System.out.println("5. Guardar y salir");
+            System.out.print("seleccione una opcion: ");
+            opcion = leer.nextInt();
+            switch(opcion){
+                case 1:
+                    System.out.println("Ingresa el nombre del atleta: ");
+                    String nombre = leer.nextLine();
+                    System.out.println("ingrese la edad del atleta: ");
+                    int edad = leer.nextInt();
+                    System.out.println("ingrese departamento del atleta: ");
+                    String depart = leer.nextLine();
+                    System.out.println("ingrese disciplina del atleta: ");
+                    String disc = leer.nextLine();
+                    System.out.println("ingrese unidad de medida del entrenemiento de atleta: ");
+                    String med = leer.nextLine();
+                    atleta atleta1 = new atleta(nombre, edad,depart,new Disciplina(disc,med));
+                    gestor.agregar(atleta1);
+                    System.out.println("atleta registrado exitosamente");
+
+                    break;
+                case 2:
+                    System.out.println("Ingresa el nombre del atleta: ");
+                    String nombreatleta = leer.nextLine();
+                    atleta a = gestor.buscar(nombreatleta);
+                    if (nombreatleta != null) {
+                        System.out.println("ingrese tipo de entrenamiento del atleta: ");
+                        String tipo = leer.nextLine();
+                        System.out.println("Ingrese valor del entrenamiento del atleta: ");
+                        double datoentreno = leer.nextDouble();
+                        leer.nextLine();
+                        a.agregarEntrenamiento(new Entrenamientos(LocalDate.now(), tipo, datoentreno));
+                        System.out.println("entrenamiento agregado exitosamente");
+                    }else{
+                        System.out.println("ateleta no existe");
+
+                    }
+                    break;
+                case 3:
+
+                    System.out.println("Ingresa el nombre del atleta: ");
+                    String nombrehistorial = leer.nextLine();
+                    atleta h = gestor.buscar(nombrehistorial);
+                    if (h != null) {
+                        estadisticas.mostrarHistorial(h);
+                    }else {
+                        System.out.println("atletea no existe");
+
+                    }
+                    
+                    break;
+                case 4:
+                    System.out.println("Ingresa el nombre del atleta: ");
+                    String nombreestadistica = leer.nextLine();
+                    atleta s = gestor.buscar(nombreestadistica);
+                    if (s != null) {
+                        System.out.println("Promedio: " + estadisticas.promedio(s));
+                        System.out.println("Mejor Marca: " + estadisticas.mejormarca(s));
+                    }else{
+                        System.out.println("atleta no existe");
+
+                    }
+                    break;
+                case 5:
+                    json.guardar(gestor.getAtletas());
+                    System.out.println("datos guardados exitosamente");
+                    break;
+                default:
+                    System.out.println("opcion invalida");
+
+            }
+
+        }while(opcion!=5);
 
     }
 }
--- src/main/java/Persistencia/GestorJSON.java
+++ src/main/java/Persistencia/GestorJSON.java
@@ -1,4 +1,56 @@
 package Persistencia;
+import Entidades.atleta;
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.io.*;
+import java.io.IOException;
+import java.util.List;
 public class GestorJSON {
+    private static final String ARCHIVO = "atletas.json";
+    private final Gson gson;
+
+    public GestorJSON() {
+        gson = new GsonBuilder().registerTypeAdapter(
+                LocalDate.class, new JsonSerializer<LocalDate>() {
+                    @Override
+                    public JsonElement serialize(LocalDate src, java.lang.reflect.Type typeOfSrc,
+                                                 JsonSerializationContext context) {
+                        return new JsonPrimitive(src.format(DateTimeFormatter.ISO_DATE));
+                    }
+                }).registerTypeAdapter(LocalDate.class, new JsonDeserializer<LocalDate>() {
+            @Override
+            public LocalDate deserialize(JsonElement json, java.lang.reflect.Type typeOfT, JsonDeserializationContext context)
+                    throws JsonParseException {
+                return LocalDate.parse(json.getAsString(), DateTimeFormatter.ISO_LOCAL_DATE);
+            }
+        }).setPrettyPrinting().create();
+
+    }
+    public void guardar(List<atleta> atletas1) {
+        try (Writer writer = new FileWriter(ARCHIVO)) {
+        new Gson().toJson(atletas1, writer);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public List<atleta> cargar() {
+    try(Reader reader = new FileReader(ARCHIVO)){
+        atleta [] atleta1 = gson.fromJson(reader, atleta[].class);
+        if (atleta1 != null){
+            List<atleta> lista   = new ArrayList<>();
+            for (atleta a : atleta1) {
+                lista.add(a);
+            }
+            return lista;
+        }
+
+    } catch (IOException e) {
+        System.out.println("no se encontro archivo JSON iniciando vacio ");
+    }
+    return new ArrayList<>();
+    }
 }
